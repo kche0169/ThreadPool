0a4f91efa007f23806d1c9bd17eb413d5d35af3f
ShiraTake <shiratakekanpakuji@gmail.com>
2024-02-24 21:28:08 +0800

    第一次提交
    将所有文件放入了github

---
+++ ThreadPoolManagement/ThreadPool/ThreadPool.cpp
@@ -0,0 +1,105 @@
+﻿// ThreadPool.cpp: 定义应用程序的入口点。
+//
+
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+#include "ThreadPool.h"
+
+//using namespace std;
+
+ThreadPool::ThreadPool() {
+	for (size_t i = 0; i < InitThreadNum; i++) {
+		// threads.emplace_back(std::bind(&ThreadPool::execute_tasks, this));
+		threads.emplace_back([this] { this->execute_tasks(); });
+
+	}
+}
+
+ThreadPool::~ThreadPool() {
+	{
+		//锁住
+		//设置CV,通知
+		std::unique_lock<std::mutex> lock(queueMutex);
+		stop = true;
+	}
+	cv.notify_all();
+
+	for (std::thread& thread : threads) {
+		thread.join();  
+	}
+}
+
+void ThreadPool::add_task(const std::function<void()>& task) {
+	
+	{
+		std::unique_lock<std::mutex> lock(queueMutex);
+		tasks.push(task);
+	}
+	cv.notify_one();
+	// 告知下一个等待操作任务队列的进程可以行动
+};
+
+
+
+void ThreadPool::execute_tasks() {
+	while (true) {
+		std::function<void()> task;
+		// 生成任务
+		
+		{
+			std::unique_lock<std::mutex> lock(queueMutex);
+			// 锁住队列
+			// 在满足条件之前释放
+			cv.wait(lock, [this] { return stop || !tasks.empty(); });
+
+			if (stop && tasks.empty()) {
+				return;
+			}
+
+			task = tasks.front();
+			tasks.pop();
+			
+		}
+
+		task();
+		// 执行任务
+	}
+}; 
+
+void ThreadPool::show_threads() {
+	for (std::thread & t : threads) {
+		std::cout << t.get_id() << std::endl;
+	}
+}
+void workThread(const int id) {
+	std::cout << "线程 "<< id << " 正在处理" << std::endl;
+}
+
+int main() {
+	ThreadPool threadPool;
+	threadPool.show_threads();
+	for (int i = 0; i < 10; ++i) {
+		if (i % 2 == 0) {
+			threadPool.add_task([i] {
+						std::cout << "Task "<< i << " executed by thread " << std::this_thread::get_id() << std::endl;
+						});
+		}
+		else {
+			threadPool.add_task([i] {
+				std::this_thread::sleep_for(std::chrono::milliseconds(200));
+				std::cout << "Task " << i << " executed by thread " << std::this_thread::get_id() << std::endl;
+				});
+		}
+		
+	}
+
+
+	// 确保所有任务执行完毕再退出
+	std::this_thread::sleep_for(std::chrono::seconds(5));
+	return 0;
+}
---
+++ ThreadPoolManagement/ThreadPool/ThreadPool.h
@@ -0,0 +1,36 @@
+﻿// ThreadPool.h: 标准系统包含文件的包含文件
+// 或项目特定的包含文件。
+
+#pragma once
+
+
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <mutex>
+#include <condition_variable>
+
+
+// TODO: 在此处引用程序需要的其他标头。
+
+class ThreadPool {
+public:
+    // 构造函数
+    ThreadPool();    
+    void add_task(const std::function<void()>& task);
+    void show_threads();
+    ~ThreadPool(); // 析构函数
+private:
+    std::queue<std::function<void()>> tasks;
+    const size_t MaxThreadNum = 5;
+    const size_t InitThreadNum = 3;
+    std::vector<std::thread> threads;
+    int tasksNum = 0;
+    bool stop = false; // 线程池是否正在运行
+    std::mutex queueMutex;
+    std::condition_variable cv;
+    void execute_tasks(); // 执行任务的主循环
+};
+
